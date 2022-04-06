f9a4196bd7c6bb2ef9ddf24473e1aabdcd92c13c
Jesus Jo <jesusjho26@gmail.com>
2022-04-06 13:46:35 -0700

    Role permission fixed

--- middleware/role.js
+++ middleware/role.js
@@ -1,20 +1,24 @@
 const { handleHttpError } = require("../utils/handleError");
-
-const checkRole = (role) => (req, res, next) => {
+/**
+ * Array con los roles permitidos
+ * @param {*} rol
+ * @returns
+ */
+const checkRole = (roles) => (req, res, next) => {
   try {
     const { user } = req;
-    console.log({ user });
-    const rolesByUser = user.role;
-    const checkValueRol = role.some((roleSinge) =>
-      rolesByUser.uncludes(roleSinge)
-    );
+    const rolesByUser = user.role; //TODO ["user"]
+    //TODO: ["admin","manager"]
+    const checkValueRol = roles.some((rolSingle) =>
+      rolesByUser.includes(rolSingle)
+    ); //TODO: true, false
     if (!checkValueRol) {
       handleHttpError(res, "USER_NOT_PERMISSIONS", 403);
       return;
     }
     next();
-  } catch (error) {
-    handleHttpError(res, "ERROR_PERMISION", 403);
+  } catch (e) {
+    handleHttpError(res, "ERROR_PERMISSIONS", 403);
   }
 };
 
--- routes/tracks.js
+++ routes/tracks.js
@@ -21,7 +21,7 @@ router.get("/", authMiddleware, getItems);
 //Get item by id
 router.get("/:id", authMiddleware, validatorGetItem, getItem);
 //Create new item
-router.post("/", authMiddleware,   checkRole(['admin']),validatorCreateItem, createItems);
+router.post("/", authMiddleware, checkRole(["user","admin"]), validatorCreateItem, createItems);
 //Update item
 router.put(
   "/:id",
